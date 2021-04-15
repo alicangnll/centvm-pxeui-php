2a781402d66e157e2363f3718f20b01ab4ba00c9
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2021-04-15 06:46:56 +0300

    CentVM PXE Boot Panel v1.1
    CentVM PXE Boot Panel v1.1

--- install.php
+++ install.php
@@ -464,6 +464,10 @@ $str3 = str_replace("System", "", $str2);
   <b> Sistem Türü : CentOS </b><br>
   <hr></hr>
   <code>
+  //For CentOS 7 Special <br>
+  yum provides semanage<br>
+  yum -y install policycoreutils-python-2.5-34.el7.x86_64<br>
+  //Finished for CentOS7 Special
   '.$installer.'<br>
   '.$mk_syslinuxfolder.'<br>
   '.$copy_syslinux.'<br>
